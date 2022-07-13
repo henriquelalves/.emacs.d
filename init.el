99d4a10f2945674503f6119b4986f96ca98ed878
Henrique Alves <henriquelalves@gmail.com>
2022-07-13 14:57:04 -0300

    Fix blank installation

--- init.el
+++ init.el
@@ -1,23 +1,10 @@
 (org-babel-load-file "~/.emacs.d/config/reduce-garbage.org")
 (org-babel-load-file "~/.emacs.d/config/better-defaults.org")
+(org-babel-load-file "~/.emacs.d/config/programming-modes.org") ;; TODO: Discover which package is messing with this script setup (an install from scratch fails if this is after helpful-packages)
 (org-babel-load-file "~/.emacs.d/config/ivy.org")
 (org-babel-load-file "~/.emacs.d/config/visuals.org")
 (org-babel-load-file "~/.emacs.d/config/functions.org")
-(org-babel-load-file "~/.emacs.d/config/programming-modes.org")
 (org-babel-load-file "~/.emacs.d/config/helpful-packages.org")
 (org-babel-load-file "~/.emacs.d/config/magit.org")
 (org-babel-load-file "~/.emacs.d/config/projectile.org")
 (org-babel-load-file "~/.emacs.d/config/roam.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-	 '(org-tracktable scala-mode php-mode lsp-pyright lsp-dart lsp-origami helm-lsp lsp-haskell lsp-python-ms lsp-java projectile magit company helpful which-key yasnippet web-mode typescript-mode slime cider clojure-mode haskell-mode rust-mode csharp-mode org-bullets rainbow-delimiters doom-themes doom-modeline all-the-icons ivy-rich counsel ivy diminish exec-path-from-shell use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
