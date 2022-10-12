aa5d84fbe60c5da316212640559e84a815990882
Henrique Alves <henriquelalves@gmail.com>
2022-10-12 18:05:46 -0300

    Latest changes

--- init.el
+++ init.el
@@ -13,6 +13,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+	 '("~/Projects/org-roam/20221006215744-aniversarios.org" "/home/peronio/Projects/org-roam/20221006202653-financeiro.org"))
  '(package-selected-packages
 	 '(org-roam projectile magit org-tracktable helpful which-key org-bullets rainbow-delimiters doom-themes doom-modeline all-the-icons ivy-rich counsel ivy diminish gdscript-mode csharp-mode typescript-mode clojure-mode scala-mode php-mode rust-mode lsp-pyright flycheck company lsp-dart lsp-origami helm-lsp lsp-haskell lsp-python-ms lsp-java yasnippet lsp-ui lsp-mode exec-path-from-shell use-package))
  '(safe-local-variable-values
