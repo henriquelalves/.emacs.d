14d7a7fab268dcd5e35c6d09c40b0ebf1bea8146
Henrique Lacreta Alves <henriquelalves@gmail.com>
2023-07-21 14:43:23 +0300

    update

--- init.el
+++ init.el
@@ -7,7 +7,6 @@
 (org-babel-load-file "~/.emacs.d/config/helpful-packages.org")
 (org-babel-load-file "~/.emacs.d/config/magit.org")
 (org-babel-load-file "~/.emacs.d/config/projectile.org")
-(org-babel-load-file "~/.emacs.d/config/roam.org")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -16,7 +15,7 @@
  '(ede-project-directories
 	 '("/home/peronio/Projects/Sailfish/Games/4-MergeWordsPuzzle"))
  '(org-agenda-files
-	 '("~/Projects/org-files/agenda/pessoal.org" "/home/peronio/Projects/org-files/agenda/financeiro.org"))
+	 '("~/Projects/org-files/agenda/financeiro.org" "/Users/macpc/Projects/org-files/agenda/pessoal.org"))
  '(package-selected-packages
 	 '(org-roam projectile magit org-tracktable helpful which-key org-bullets rainbow-delimiters doom-themes doom-modeline all-the-icons ivy-rich counsel ivy diminish gdscript-mode csharp-mode typescript-mode clojure-mode scala-mode php-mode rust-mode lsp-pyright flycheck company lsp-dart lsp-origami helm-lsp lsp-haskell lsp-python-ms lsp-java yasnippet lsp-ui lsp-mode exec-path-from-shell use-package))
  '(safe-local-variable-values
